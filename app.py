0c828bf1eb65386c8e581db0f66be9d604282583
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-06-26 00:35:54 -0500

    added connect_db from models

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from sqlalchemy
 
+from models import connect_db()
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = (
     os.environ.get('DATABASE_URL', 'postgres:///ammo_surplus'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
+
+connect_db(app)

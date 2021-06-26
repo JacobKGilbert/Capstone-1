d67307c3f7b407a99e37fbb6a7114452d5188e84
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-06-26 15:46:06 -0500

    created AddCartForm

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms import StringField, PasswordField, IntegerField
+from wtforms.validators import DataRequired, Email, Length, NumberRange
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +8,9 @@ class LoginForm(FlaskForm):
 
   email = StringField('Email', validators=[DataRequired(), Email()])
   password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+
+class AddCartForm(FlaskForm):
+  '''Form for adding product to cart.'''
+
+  quantity = IntegerField('Qty', validators=[DataRequired(), NumberRange(min=1, max=20, message='Minimum quantity is 1. Maximum quantity is 20.')])
+

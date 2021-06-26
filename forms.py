24d90500a25cc02b8df2d5cfc5d63c60e6788f8c
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-06-26 15:40:58 -0500

    created login form

--- forms.py
+++ forms.py
@@ -1,3 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, Email, Length
+
+
+class LoginForm(FlaskForm):
+  '''Form for the login process'''
+
+  email = StringField('Email', validators=[DataRequired(), Email()])
+  password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])

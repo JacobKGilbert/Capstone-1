961457ece7b843f194d41bc3539f5204f08f2809
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-07-05 17:16:51 -0500

    edited login form name to LoginSignupForm

--- forms.py
+++ forms.py
@@ -3,8 +3,8 @@ from wtforms import StringField, PasswordField, IntegerField
 from wtforms.validators import DataRequired, Email, Length, NumberRange
 
 
-class LoginForm(FlaskForm):
-  '''Form for the login process'''
+class LoginSignupForm(FlaskForm):
+  '''Form for the login/signup process'''
 
   email = StringField('Email', validators=[DataRequired(), Email()])
   password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])

fd53424d72d5980f890cbca02538cb8c55683c17
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-07-05 17:17:24 -0500

    commented out login and register route

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from models import db, connect_db
+from forms import LoginSignupForm
 import os
 
 app = Flask(__name__)
@@ -10,3 +11,26 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 
 connect_db(app)
+
+@app.route('/')
+def home():
+    '''Get the home template.'''
+    
+    return render_template('cart.html')
+
+
+# @app.route('/login', methods=['POST'])
+# def login():
+#     '''Handle Login'''
+#     req = request.get_json()
+
+#     form = ValidateForm(obj=req, meta={'csrf': False})
+#     if request.method == 'POST' and form.validate():
+
+
+
+# @app.route('/register', methods=['POST'])
+# def register():
+#     '''Handle registering new account.'''
+
+

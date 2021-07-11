840aa94961757aee358e0894edb79bf772da23de
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-07-10 21:25:28 -0500

    added Square SDK function

---
+++ static/javascript/square_sdk.js
@@ -0,0 +1,31 @@
+// Pulled from the Square Web SDK
+APPLICATION_ID = 'sandbox-sq0idb-nmDwaVz9m2bkzGGK_dQJsg'
+LOCATION_ID = 'LXD3BVFHEVCS0'
+
+async function main() {
+  const payments = Square.payments(APPLICATION_ID, LOCATION_ID)
+
+  const card = await payments.card()
+
+  await card.attach('#card-container')
+
+  async function eventHandler(event) {
+    event.preventDefault()
+
+    try {
+      const result = await card.tokenize()
+
+      if (result.status === 'OK') {
+        console.log(`Payment token is ${result.token}`)
+      }
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  const cardButton = document.getElementById('card-button')
+
+  cardButton.addEventListener('click', eventHandler)
+}
+
+main()

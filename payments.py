eaacdb11038913183702b2f09c882c86ae1fb7c1
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-07-22 12:07:31 -0500

    pulled from square docs for python version of square sdk

---
+++ payments.py
@@ -0,0 +1,38 @@
+from square.client import Client
+from dotenv import load_dotenv
+import os
+import uuid
+
+# Load environment variables from .env file
+load_dotenv()
+
+def make_payment(token, amount, order_id):
+  # Create an instance of the API Client
+  # and initialize it with the credentials
+  # for the Square account whose assets you want to manage
+  client = Client(
+      access_token=os.environ.get('SQUARE_ACCESS_TOKEN'),
+      environment='sandbox',
+  )
+
+  # Create an idempotency key to ensure no duplication of payments. Using uuid4 we ensure the randomness of the number while also ensuring the absolute privacy of the key.
+  idempotency_key = str(uuid.uuid4())
+
+  # Get an instance of the Square Payments API
+  payments_api = client.payments
+
+  body = {}
+  body['source_id'] = token
+  body['idempotency_key'] = idempotency_key
+  body['amount_money'] = {}
+  body['amount_money']['amount'] = amount
+  body['amount_money']['currency'] = 'USD'
+  body['order_id'] = order_id
+  body['location_id'] = os.environ.get('LOCATION_ID')
+
+  result = payments_api.create_payments(body)
+
+  if result.is_success():
+    print(result.body)
+  elif result.is_error():
+    print(result.errors)

9f1fa336af4fd7827385be6e3940b80154ff0915
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-07-29 13:32:00 -0500

    added 'let' before each variable... because this isn't python

--- static/javascript/a.js
+++ static/javascript/a.js
@@ -2,12 +2,12 @@
 
 /** Adjusts total price due based on current cart items. */ 
 function changeTotalPayment() {
-  totalElement = $('#amount-total')
-  subTotals = $('.subtotal')
-  totalAmount = 0
+  let totalElement = $('#amount-total')
+  let subTotals = $('.subtotal')
+  let totalAmount = 0
 
   for (const subtotal of subTotals) {
-    subT = parseFloat($(subtotal).text())
+    let subT = parseFloat($(subtotal).text())
 
     totalAmount += subT
   }
@@ -28,12 +28,12 @@ function removeProductFromCart(cartProduct) {
 
 /** Adjusts the quantity and subtotal of the input and subtotal span, respectively. Then calls changeTotalPayment */
 function updateCart(productID, quantity, price) {
-  cartFormInput = $(`#qty-${productID}`)
-  cartItemSubtotal = $(`#product${productID}-subtotal`)
+  let cartFormInput = $(`#qty-${productID}`)
+  let cartItemSubtotal = $(`#product${productID}-subtotal`)
 
   cartFormInput.val(quantity)
 
-  subtotal = quantity * price
+  let subtotal = quantity * price
   cartItemSubtotal.text(`${subtotal}`)
 
   changeTotalPayment()
@@ -41,11 +41,11 @@ function updateCart(productID, quantity, price) {
 
 /** Creates cart item and adds it to cart. Also adds submit trigger to created item's form. */
 function addProductToCart(productID, quantity, price) {
-  title = $(`#product-id${productID}`).text()
-  subtotal = quantity * price
+  let title = $(`#product-id${productID}`).text()
+  let subtotal = quantity * price
 
   // Create cart item
-  cartItem = $(`
+  let cartItem = $(`
       <div id='item-id${productID}'>
       <p class='card-text'>${title}</p>
       <div class="row g-0">
@@ -75,13 +75,13 @@ function addProductToCart(productID, quantity, price) {
   $(`#item-id${productID} form`).submit(function (evt) {
     evt.preventDefault()
 
-    form = evt.currentTarget
-    input = form[0]
-    productID = $(form).attr('id')
-    quantity = parseFloat($(input).val())
-    price = parseFloat($(`#product-price${productID}`).text())
-    cartProduct = form.closest(`#item-id${productID}`)
-    catalogProduct = $(`#product${productID}div`)
+    let form = evt.currentTarget
+    let input = form[0]
+    let productID = $(form).attr('id')
+    let quantity = parseFloat($(input).val())
+    let price = parseFloat($(`#product-price${productID}`).text())
+    let cartProduct = form.closest(`#item-id${productID}`)
+    let catalogProduct = $(`#product${productID}div`)
 
     if (quantity > 0) {
       updateCart(productID, quantity, price)
@@ -97,12 +97,12 @@ function addProductToCart(productID, quantity, price) {
 $('.product-form').submit(function (evt) {
   evt.preventDefault()
 
-  form = evt.currentTarget
-  input = form[0]
-  productID = $(form).attr('id')
-  quantity = parseFloat($(input).val())
-  price = parseFloat($(`#product-price${productID}`).text())
-  product = form.closest('.col')
+  let form = evt.currentTarget
+  let input = form[0]
+  let productID = $(form).attr('id')
+  let quantity = parseFloat($(input).val())
+  let price = parseFloat($(`#product-price${productID}`).text())
+  let product = form.closest('.col')
 
   $('.empty-cart-message').hide()
   addProductToCart(productID, quantity, price)

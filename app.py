3a52accc1d0c1980132bbebce8c26b05bbfd4feb
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-07-30 18:03:40 -0500

    added handling for sqlalchemy's 'postgresql' preference

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = (
     os.environ.get('DATABASE_URL', 'postgresql:///ammo_surplus'))
+# Heroku uses 'postgres' while sqlalchemy (version 1.4) only allows 'postgresql'
+if app.config['SQLALCHEMY_DATABASE_URI'].startswith("postgres://"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = app.config['SQLALCHEMY_DATABASE_URI'].replace(
+        "postgres://", "postgresql://", 1)
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "Acts2:38")
